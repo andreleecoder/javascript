d0f09e3495fe06b1986e41d67b3e697370c2b512
Andre <andreleecoder@gmail.com>
2019-05-07 08:42:21 -0300

    enviroment seted

---
+++ appMataMosquitp/jogo.js
@@ -0,0 +1,9 @@
+var altura=0
+var largura =0
+//captar o tamanho da tela do browser
+ 
+function ajustaTamanhoTela(){
+    altura=window.innerHeight 
+    largura=window.innerWidth
+    console.log(altura,largura)
+ }
