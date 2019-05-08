9faeb3ad66ea7625b51a540c7a74374b20d9f06a
Andre Lee <andreleecoder@gmail.com>
2019-05-07 22:23:19 -0300

    cenario, mosquito e paineis done

--- appMataMosquitp/jogo.js
+++ appMataMosquitp/jogo.js
@@ -10,6 +10,10 @@ function ajustaTamanhoTela(){
  ajustaTamanhoTela()
 // movimentação aleatoria do mosquito
  function posicaoRandomica(){
+     //remover o mosquito anterior (caso exista)
+     if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+     }
      var posicaox=Math.floor(Math.random() * largura)-90
      var posicaoy=Math.floor(Math.random() * altura)-90
      
@@ -26,16 +30,38 @@ function ajustaTamanhoTela(){
      //criando elemento html dentro do javascript
      var mosquito=document.createElement('img')
      mosquito.src='./imagens/mosca.png'
-     mosquito.className='mosquito1'
+     mosquito.className=tamanhoAleatorio()+ ' '+ladoAleatorio()
 
      mosquito.style.left=posicaox +'px'
      mosquito.style.top=posicaoy +'px'
      mosquito.style.position='absolute'
-     
+     mosquito.id='mosquito'
 
      mosquito.style.left=posicaox+'px'
      mosquito.style.top=posicaoy+'px'
      mosquito.style.position='absolute'
 
      document.body.appendChild(mosquito)
+ }
+ //Alterando o tamanho do mosquito aleatoriamanete
+ function tamanhoAleatorio(){
+     var classe = Math.floor(Math.random()*3)
+     switch(classe){
+         case 0:
+         return 'mosquito1'
+         case 1:
+         return 'mosquito2'
+         case 2:
+         return 'mosquito3'
+     }
+ }
+ //espelhando a imagem do mosquito
+ function ladoAleatorio(){
+     var classe = Math.floor(Math.random()*2)
+     switch(classe){
+         case 0:
+         return 'ladoA'
+         case 1:
+         return 'ladoB'
+     }
  }
