427fece270528117a5b8f798b85dcd213db15b4d
Andre Lee <andreleecoder@gmail.com>
2019-09-23 16:52:07 -0300

    aula 0

---
+++ algoritmos/livro/aula-0.js
@@ -0,0 +1,28 @@
+/*var myVariable="global";
+var myOtherVariable="global";
+
+function myFunction(){
+    var myVariable= "local";
+    return myVariable
+}
+
+
+function myOtherFunction(){
+    var myOtherVariable= "local";
+    return myOtherVariable
+}
+
+console.log(myVariable);
+console.log(myFunction());
+console.log(myOtherVariable);
+console.log(myOtherFunction());
+*/
+var i=0;
+
+
+while (i < 11) {
+console.log(i);
+i++;
+    
+}
+
