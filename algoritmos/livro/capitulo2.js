0e133060debe4b17a262360ec662185d6e427393
Andre Lee <andreleecoder@gmail.com>
2019-09-30 20:00:16 -0300

    arrow functions

--- algoritmos/livro/capitulo2.js
+++ algoritmos/livro/capitulo2.js
@@ -1,4 +1,4 @@
-let movie ='Lord of the Rings'
+/*let movie ='Lord of the Rings'
 //var movie= 'Batman VS Superman'
 function StarWarsFan(){
     const movie='Star Wars'
@@ -26,6 +26,56 @@ function blizzardFan() {
     console.log(marvelFan());
     console.log(movie);
     blizzardFan()
+//TEMPLATE LITERAL
+    
+//concatenação
+    const livro={
+        nome:'Estruturas de dados e algoritmos com javaScritpt'
+    };
+    //console.log('voce está lendo' + livro.nome);
+
+//template literal
+console.log(`voce está lendo ${livro.nome}., 
+and this is a new line
+and so this` );
+
+//ARROW FUNCTIONS
+
+//função tradicional
+var circleAreaEs5 = function circleAreaEs5(r) {
+    var PI=3.14
+    var area=PI*r*r
+    return area    
+}
+console.log(circleAreaEs5(2));
+
+//arrow function
+let circleArea = r=>{
+    const PI=3.14
+    const area=PI*r*r
+    return area 
+}
+console.log(circleArea(3));
+*/
+function repeatStringNumTimes(str, num) {
+    // repeat after me
+   let newStr=''
+   while (num>0) {
+       newStr+=str
+       num--
+       
+   }
+ 
+    return newStr;
+  }
+  
+ console.log( repeatStringNumTimes("abc", 6));
+ 
+
+
+
+    
+
     
     
     
