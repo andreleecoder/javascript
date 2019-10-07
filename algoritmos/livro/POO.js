57135c4d31e7e9c78e2d8271a8a61f8f85d9a3b1
Andre Lee <andreleecoder@gmail.com>
2019-10-07 12:51:13 -0300

    modulos

--- algoritmos/livro/POO.js
+++ algoritmos/livro/POO.js
@@ -1,77 +1,11 @@
-// programação orientada a objetos com classes
-/*class Book{
-    constructor(title,pages,isbn ){
-        this.title=title
-        this.pages=pages
-        this.isbn=isbn
-    }
-    printisbn(){
-        console.log(this.isbn);
-        
-    }
+import {circleArea , squareArea} from './modulos';
 
-}
-let book=new Book('titulo','paginas','isbn')
-//console.log(book.printisbn('não é'));
+    console.log(circleArea(2))
 
-//herança
-class ItBook extends Book{
-    constructor(title,pages,isbn,technology){
-    super(title,pages,isbn)
-    this.technology=technology
-}
-printTechnology(){
-    console.log(this.technology)
-    
-    }
 
-}
 
-let jsBook=new ItBook('Learning JS algorithms','200','1234567890','JavaScript')
-//console.log(jsBook.title)
-//console.log(jsBook.printTechnology());
-//getters and setters
 
-class Person{
-    constructor(name){
-        this._name=name
-    }
-    get name(){
-        return this._name
-    }
-    set name(value){
-        this._name=value
-    }
-}
 
-let lotchar=new Person('Frodo')
-console.log(lotchar.name)
-lotchar.name='Gandalf'
-console.log(lotchar.name)
-lotchar._name='Sam'
-console.log(lotchar.name)
-
-//funões getters and setters em classes
-
-class Person{
-    constructor(name){
-        this._name=name
-    }
-    get name(){
-        return this._name
-    }
-    set name(value){
-        return this._name=value
-    }
-}
-let lotChar= new Person('Frodo')
-console.log(lotChar.name);
-lotChar.name='Gandalf'
-console.log(lotChar.name);
-lotChar.name='Sam'
-console.log(lotChar.name);
-*/
-//operador exponencial
 
 
 
